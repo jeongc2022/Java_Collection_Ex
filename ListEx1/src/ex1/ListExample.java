fe00862426d2ef05a703fe79126b5032ad1822f4
Jeongc2022 <jeongc2022@gmail.com>
2022-01-11 11:36:28 +0900

    1. List Example

--- ListEx1/src/ex1/ListExample.java
+++ ListEx1/src/ex1/ListExample.java
@@ -9,6 +9,7 @@ public class ListExample {		// BoardDao 사용자 클래스입니다.
 		for(Board board : list) {
 			System.out.println(board.getTitle() + "-" + board.getContent());
 		}
+		
 	}
 }
 
@@ -16,4 +17,8 @@ public class ListExample {		// BoardDao 사용자 클래스입니다.
  * BoardDao 객체의 getBoardList() 메소드를 호출하면 List<Board> 타입의 컬렉션을 리턴합니다.
  * ListExample 클래스를 실행시켰을 때 다음과 같이 출력될 수 있도록 BoardDao의 getBoardList() 메소드를
  * 작성해보세요.
+ * 
+ * 제목1-내용1
+ * 제목2-내용2
+ * 제목3-내용3
 */
