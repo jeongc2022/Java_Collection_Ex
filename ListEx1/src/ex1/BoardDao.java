bd2865c2f3cee3b514bfdc80961f59f489d6eae1
Jeongc2022 <jeongc2022@gmail.com>
2022-01-12 13:14:30 +0900

    1. List Example Answer

--- ListEx1/src/ex1/BoardDao.java
+++ ListEx1/src/ex1/BoardDao.java
@@ -7,7 +7,7 @@ public class BoardDao {			// 게시물을 가져오는 클래스
 
 	// 정답을 적어주세요.
 	public List<Board> getBoardList() {
-		List<Board> list = new ArrayList();
+		List<Board> list = new ArrayList<Board>();
 		list.add(new Board("제목1", "내용1"));
 		list.add(new Board("제목2", "내용2"));
 		list.add(new Board("제목3", "내용3"));
